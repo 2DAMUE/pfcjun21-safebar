c06fa776ff36646dc9034dc2c7750904be396316
ccapu <c.capuchino@hotmail.com>
2021-04-18 16:09:50 +0200

    Cuenta

--- app/src/main/java/com/dam/safebar/Cuenta.java
+++ app/src/main/java/com/dam/safebar/Cuenta.java
@@ -2,7 +2,14 @@ package com.dam.safebar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.dam.safebar.fragments.AboutUs;
+import com.dam.safebar.fragments.Ayuda;
+import com.dam.safebar.fragments.Configuracion;
+import com.dam.safebar.fragments.Protocolo_Covid;
 
 public class Cuenta extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class Cuenta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta);
+    }
+
+    public void Configuracion(View view) {
+        Intent i =new Intent(this, Configuracion.class);
+        startActivity(i);
+
+    }
+
+    public void AboutUs(View view) {
+        Intent i =new Intent(this, AboutUs.class);
+        startActivity(i);
+    }
+
+    public void Ayuda(View view) {
+        Intent i =new Intent(this, Ayuda.class);
+        startActivity(i);
+    }
+
+    public void Protocolo(View view) {
+        Intent i =new Intent(this, Protocolo_Covid.class);
+        startActivity(i);
+
     }
 }
